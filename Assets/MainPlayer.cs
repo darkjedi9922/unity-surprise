dc45e3c71254ebf7bc26ec54a929973e1eec79f0
Jed Dark <darkjedi9922@gmail.com>
2019-10-15 18:42:40 +0300

    Surprise, Unity.

---
+++ Assets/MainPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    //private Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, Unity");
+        //transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vValue = Input.GetAxis("Vertical");
+        float hValue = Input.GetAxis("Horizontal");
+   
+        transform.Translate(0, 0, vValue / (float) 5);
+        transform.Rotate(0, hValue * 2, 0);
+    }
+}
