310bff7a4455476e207679c6cb6304597e65fe2d
Jed Dark <darkjedi9922@gmail.com>
2019-10-15 19:30:47 +0300

    Add lava and death sounds.

---
+++ Assets/WhitePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhitePlayer : MonoBehaviour
+{
+    AudioSource falling;
+    bool failed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        falling = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!failed && transform.position.y < -5)
+        {
+            Debug.Log(transform.position.y);
+            falling.Play(0);
+            failed = true;
+        }
+    }
+}
